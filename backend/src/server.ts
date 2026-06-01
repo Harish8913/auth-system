895b9e852a045f9d84ff61e4b8af8333f9630af0
Harish Puri <harishpuri.in@gmail.com>
2026-06-01 23:18:29 +0530

    Auth System Start

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import type { Express, Request, Response } from "express";
+import express from "express";
+import config from "./config/config.js";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+app.listen(config.port, () => {
+  console.log(
+    `Server Running on port: ${config.port} in ${config.nodeEnv} mode`,
+  );
+});
