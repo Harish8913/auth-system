173b201317826b0251ad95254660e1a05701dc75
Harish Puri <harishpuri.in@gmail.com>
2026-06-02 10:24:46 +0530

    Server run initial

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,15 +1,42 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-interface Config {
+interface ServerConfig {
   port: number;
   nodeEnv: string;
 }
 
-const config: Config = {
-  port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
+interface DbConfig {
+  user: string;
+  host: string;
+  database: string;
+  password: string;
+  port: number;
+}
+
+interface AppConfig {
+  server: ServerConfig;
+  db: DbConfig;
+}
+
+const requireEnv = (name: string): string => {
+  const value = process.env[name];
+  console.log(name, value);
+  if (!value) throw new Error(`Missing Environment Variable: ${name}`);
+  return value;
 };
 
-export default config;
+export const config: AppConfig = {
+  server: {
+    port: Number(process.env.PORT) || 3000,
+    nodeEnv: process.env.NODE_ENV || "development",
+  },
+
+  db: {
+    user: requireEnv("DB_USER"),
+    host: requireEnv("DB_HOST"),
+    database: requireEnv("DB_NAME"),
+    password: requireEnv("DB_PASSWORD"),
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+  },
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,28 @@
+console.log('ENV CHECK: ', process.env.DB_USER)
 import type { Express, Request, Response } from "express";
-import express from "express";
-import config from "./config/config.js";
+import express, { response } from "express";
+import { config } from "./config/config.js";
+import { Pool } from "pg";
 
 const app: Express = express();
+
+const pool = new Pool(config.db);
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(config.port, () => {
+app.use("/index", async (req: Request, res: Response) => {
+  try {
+    const results = await pool.query("SELECT * FROM users");
+    response.status(200).json(results.rows);
+  } catch (err) {
+    throw err;
+  }
+});
+
+app.listen(config.server.port, () => {
   console.log(
-    `Server Running on port: ${config.port} in ${config.nodeEnv} mode`,
+    `Server Running on port: ${config.server.port} in ${config.server.nodeEnv} mode`,
   );
 });
