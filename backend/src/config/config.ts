c5210add1ba15b06b0f102375dedf1ffb8b19eb4
Harish Puri <harishpuri.in@gmail.com>
2026-06-02 16:12:03 +0530

    Postgresql connected using prisma

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,42 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 interface ServerConfig {
   port: number;
   nodeEnv: string;
 }
 
-interface DbConfig {
-  user: string;
-  host: string;
-  database: string;
-  password: string;
-  port: number;
-}
-
 interface AppConfig {
   server: ServerConfig;
-  db: DbConfig;
 }
 
-const requireEnv = (name: string): string => {
-  const value = process.env[name];
-  console.log(name, value);
-  if (!value) throw new Error(`Missing Environment Variable: ${name}`);
-  return value;
-};
-
 export const config: AppConfig = {
-  server: {
+  server: { 
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || "development",
-  },
-
-  db: {
-    user: requireEnv("DB_USER"),
-    host: requireEnv("DB_HOST"),
-    database: requireEnv("DB_NAME"),
-    password: requireEnv("DB_PASSWORD"),
-    port: parseInt(process.env.DB_PORT || "5432", 10),
-  },
+  }
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,53 @@
-console.log('ENV CHECK: ', process.env.DB_USER)
 import type { Express, Request, Response } from "express";
 import express, { response } from "express";
 import { config } from "./config/config.js";
-import { Pool } from "pg";
+import cors from "cors";
+import { prisma } from "./lib/prisma.js";
 
 const app: Express = express();
 
-const pool = new Pool(config.db);
+app.use(cors());
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/index", async (req: Request, res: Response) => {
-  try {
-    const results = await pool.query("SELECT * FROM users");
-    response.status(200).json(results.rows);
-  } catch (err) {
-    throw err;
-  }
-});
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World",
+          content: "This is my first post!",
+          published: true,
+        },
+      },
+    },
+    include: {
+      posts: true,
+    },
+  });
+  console.log("Created user:", user);
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 app.listen(config.server.port, () => {
   console.log(
