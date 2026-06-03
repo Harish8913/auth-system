d5570727752331bd863b75f721467ba7e19bf2d8
Harish Puri <harishpuri.in@gmail.com>
2026-06-04 00:01:36 +0530

    L2

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,3 @@
+import type { Request, Response } from "express";
+
+export const registerController = async (req: Request, res: Response) => {};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import type { Express, Request, Response } from "express";
-import express, { response } from "express";
+import express from "express";
 import { config } from "./config/config.js";
 import cors from "cors";
-import { prisma } from "./lib/prisma.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app: Express = express();
 
@@ -12,42 +12,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-      posts: {
-        create: {
-          title: "Hello World",
-          content: "This is my first post!",
-          published: true,
-        },
-      },
-    },
-    include: {
-      posts: true,
-    },
-  });
-  console.log("Created user:", user);
-  // Fetch all users with their posts
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.log("All users:", JSON.stringify(allUsers, null, 2));
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+app.use("/api", authRoutes);
 
 app.listen(config.server.port, () => {
   console.log(
