47f706d54431c059fb1f25689046414951a7753b
Harish Puri <harishpuri.in@gmail.com>
2026-09-24 10:09:49 +0530

    @

--- backend/src/controllers/health.controller.ts
+++ backend/src/controllers/health.controller.ts
@@ -2,4 +2,3 @@ import type { Request, Response } from "express";
 export const healthController = async (req: Request, res: Response) => {
   return res.status(200).json({ message: "Server is healthy" });
 };
-  
